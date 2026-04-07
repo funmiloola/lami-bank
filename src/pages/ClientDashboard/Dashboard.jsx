9ef7a7e545df291e0e872bd86a77f0a3f240f5db
unknown <funmilolaadebiyi52@gmail.com>
2026-04-07 11:46:39 +0100

    ui-updates

--- src/pages/ClientDashboard/Dashboard.jsx
+++ src/pages/ClientDashboard/Dashboard.jsx
@@ -8,40 +8,52 @@ export default function Dashboard() {
       title: "Active Clients",
       value: "240",
       icn: "/icon (9).svg",
-          perc: "5%",
-      change:"compared to last month"
+      perc: "5%",
+      change: "compared to last month",
     },
     {
       icon: "/badge-check.svg",
       title: "Verifications",
       value: "12,070",
       icn: "/icon (9).svg",
-        perc: "9%",
-       change:"compared to last month"
+      perc: "9%",
+      change: "compared to last month",
     },
     {
       icon: "/file-stack.svg",
       title: "API Calls",
       value: "35,760",
       icn: "/icon (9).svg",
-        perc: "3%",
-       change:"compared to last month"
+      perc: "3%",
+      change: "compared to last month",
     },
   ];
   return (
     <section>
       <div className="bg-[#101928] border my-3.75 mx-3.5 rounded-lg font-sans px-5.75">
-        <Header className={`text-white`}/>
+        <Header className={`text-white`} />
         <div className="pt-6 pb-4">
           <HeaderSection>
             <div className=" flex gap-4 items-center">
               <div className="flex gap-2.5 md:gap-5 items-center">
-                <img src="/home (1).svg" alt="" className="w-3 h-3 md:w-5 md:h-5"/>
-                <span className="text-white text-xs md:text-base">Dashboard</span>
+                <img
+                  src="/home (1).svg"
+                  alt=""
+                  className="w-3 h-3 md:w-5 md:h-5"
+                />
+                <span className="text-white text-xs md:text-base">
+                  Dashboard
+                </span>
               </div>
               <div className="flex gap-2.5 md:gap-5 items-center">
-                <img src="/Vector (29).svg" alt=""  className="w-3 h-3 md:w-5 md:h-5"/>
-                <span className="text-white text-xs md:text-base">Client Management</span>
+                <img
+                  src="/Vector (29).svg"
+                  alt=""
+                  className="w-3 h-3 md:w-5 md:h-5"
+                />
+                <span className="text-white text-xs md:text-base">
+                  Client Management
+                </span>
               </div>
             </div>
           </HeaderSection>
@@ -54,26 +66,29 @@ export default function Dashboard() {
                 Track and manage your client here
               </p>
 
-              <div className="bg-[#45B2C7] flex flex-col md:flex-row items-center gap-8 rounded-[16px] px-[15px] py-[17px] md:px-[27px] md:py-[25px] mt-5 md:mt-10">
+              <div className="bg-[#45B2C7] flex flex-col md:flex-row items-center gap-6 rounded-[16px] px-[15px] py-[17px]  xl:px-[20px] md:py-[25px] mt-5 md:mt-10">
                 <div>
-                  <h3 className="text-white font-medium text-lg">
+                  <h3 className="text-white font-medium text-base xl:text-lg">
                     All your Clients in One Place
                   </h3>
-                  <p className="text-white pt-3">
+                  <p className="text-white text-sm xl:text-base pt-3">
                     Keep track of all your clients and <br /> manage all their
                     activities.
                   </p>
                 </div>
 
-                <button className="bg-white text-[#45B2C7] py-2 px-6 rounded-lg cursor-pointer">
+                <button className="bg-white text-[#45B2C7] text-xs py-2 px-3 xl:px-6 rounded-lg cursor-pointer">
                   Add New Client
                 </button>
               </div>
             </div>
             <div>
-              <div className="grid grid-cols-1 w-[270px] md:w-full  md:grid-cols-3 gap-6 md:gap-4 ">
+              <div className="grid grid-cols-1 w-[270px] md:w-full  md:grid-cols-3 gap-6 md:gap-3 xl:gap-3 ">
                 {stats.map((stat, index) => (
-                  <div key={index} className="bg-white  px-4 py-13 rounded-[12px]">
+                  <div
+                    key={index}
+                    className="bg-white  px-4 py-13 rounded-[12px]"
+                  >
                     <div className="">
                       <img
                         src={stat.icon}
@@ -87,13 +102,15 @@ export default function Dashboard() {
                     <h2 className="text-[#344054] font-medium text-[24px] pt-2">
                       {stat.value}
                     </h2>
-                         <div className="flex gap-1.5 pt-2">
-                        <div className="bg-[#E7F6EC] rounded-[10px] px-0.5 flex gap-0.5 items-center text-xs font-medium">
-                            <img src={stat.icn} alt="" />
-                           <span> {stat.perc} </span>
-                            </div>
-                            <span className="text-[10px] text-[#7A7A9D]">{ stat.change}</span>
-                            </div>
+                    <div className="flex gap-1.5 pt-2">
+                      <div className="bg-[#E7F6EC] rounded-[10px] px-0.5 flex gap-0.5 items-center text-xs font-medium">
+                        <img src={stat.icn} alt="" />
+                        <span> {stat.perc} </span>
+                      </div>
+                      <span className="text-[10px] text-[#7A7A9D]">
+                        {stat.change}
+                      </span>
+                    </div>
                   </div>
                 ))}
               </div>
--- src/pages/Dashboard/Table.jsx
+++ src/pages/Dashboard/Table.jsx
@@ -1,4 +1,4 @@
-import Page from '../../components/Pagination'
+import Page from "../../components/Pagination";
 const applicants = [
   {
     name: "Oluwatobi Adeyemi",
@@ -44,10 +44,14 @@ const applicants = [
 export default function Table() {
   return (
     <section className="mx-8 my-8 pt-4 border border-[#E2E4E9] rounded-[8px] ">
-      <div className="flex flex-col gap-6 md:gap-0  justify-between md:items-center px-4">
+      <div className="flex flex-col md:flex-row gap-6 md:gap-0  justify-between md:items-center px-4">
         <div className="flex gap-3 border border-[#E2E4E9] p-2 rounded-[8px] w-full md:w-[35%]">
           <img src="/Vector (25).svg" alt="search-icon" />
-          <input type="text" placeholder="Search..." className="outline-none w-full"/>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="outline-none w-full"
+          />
         </div>
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2 border border-[#E2E4E9] p-2 rounded-[8px] cursor-pointer">
@@ -62,19 +66,21 @@ export default function Table() {
         <table className="w-full text-sm">
           <thead className="text-[#475467] font-medium text-left bg-[#E4E7EC]">
             <tr>
-              <th  className="py-3 px-6">Applicant</th>
-              <th  className="py-3">Reference Number</th>
-              <th  className="py-3">BVN</th>
-              <th  className="py-3">ID Type</th>
-              <th  className="py-3">Status</th>
-              <th  className="py-3">Submission Date</th>
+              <th className="py-3 px-6">Applicant</th>
+              <th className="py-3">Reference Number</th>
+              <th className="py-3">BVN</th>
+              <th className="py-3">ID Type</th>
+              <th className="py-3">Status</th>
+              <th className="py-3">Submission Date</th>
             </tr>
           </thead>
 
           <tbody>
             {applicants.map((item, index) => (
               <tr key={index} className="border-b border-b-[#E4E7EC]">
-                <td className="text-[#407BFF] pl-6 pr-4 py-4 underline">{item.name}</td>
+                <td className="text-[#407BFF] pl-6 pr-4 py-4 underline">
+                  {item.name}
+                </td>
                 <td className=" py-4 pr-4 text-[#475467]">{item.ref}</td>
                 <td className="py-4 pr-4 text-[#475467]">{item.bvn}</td>
                 <td className="py-4 pr-4 text-[#475467]">{item.idType}</td>
@@ -96,8 +102,8 @@ export default function Table() {
             ))}
           </tbody>
         </table>
-          </div>
-            <Page/>
+      </div>
+      <Page />
     </section>
   );
 }
